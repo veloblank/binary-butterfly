16bbc204e7c49fc9b708fcfd89008fdfae978421
John Blank <veloblank@gmail.com>
2019-11-15 19:48:06 -0500

    Build index for nested week/props request

--- app/controllers/api/v1/contest_props_controller.rb
+++ app/controllers/api/v1/contest_props_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::ContestPropsController < ApplicationController
+
+  def index
+    contest_week = ContestWeek.find_by(:id => params[:contest_week_id])
+    if contest_week
+      contest_props = ContestProp.filter_by_contest_week(contest_week)
+    else
+      contest_props = ContestProp.all
+    end
+
+    render json: contest_props, status: 200
+
+  end
 end
