fe0269a545d2a897579d3b16dd1a3606df931aba
John Blank <veloblank@gmail.com>
2019-11-15 19:09:22 -0500

    Namespace controller and nest routes for show/ind

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :contest_props
   namespace :api do 
     namespace :v1 do 
-      resources :contest_weeks
+      resources :contest_weeks, only: [:show, :index] do
+        resources :contest_props, only: [:show, :index]
+      end
+      resources :contest_props, only: [:index, :show, :new, :create, :edit, :update]
     end
   end
 end
