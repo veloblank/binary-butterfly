495fa472784c93ef94051cec9c823c91562bf0d4
John Blank <veloblank@gmail.com>
2019-11-15 19:08:27 -0500

    Seed data with scraper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-ContestWeek.create([
-  {name: "Week 1"},
-  {name: "Week 2"},
-  {name: "Week 3"},
-  {name: "Week 4"},
-  {name: "Week 5"},
-  {name: "Week 6"},
-  {name: "Week 7"},
-  {name: "Week 8"},
-  {name: "Week 9"},
-  {name: "Week 10"},
-  {name: "Week 11"},
-  {name: "Week 12"},
-  {name: "Week 13"},
-  {name: "Week 14"},
-  {name: "Week 15"},
-  {name: "Week 16"}
-  ])
+espn = "http://fantasy.espn.com/streak/en/"
+doc = Nokogiri::HTML(open(espn))
+props = doc.css(".matchup-container")
+
+props.each do |p|
+  date = p.css(".startTime").attr("data-locktime").value
+  contest_week = ContestWeek.find_or_create_by(name: "#{Date.today.cweek}")
+  ContestProp.create(
+    title:  p.css(".gamequestion").text,
+    date: DateTime.parse(date).in_time_zone('Eastern Time (US & Canada)').to_date,
+    start_time: DateTime.parse(date).in_time_zone('Eastern Time (US & Canada)'),
+    sport: p.css(".sport-description").text,
+    away_team: p.css("td span strong")[0].text,
+    home_team: p.css("td span strong")[1].text,
+    contest_week_id: contest_week.id
+  )
+
+
+end
