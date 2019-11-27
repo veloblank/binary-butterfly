41c75c82de87f9b4dd86743a1de5e9bd454e658a
John Blank <veloblank@gmail.com>
2019-11-27 16:47:05 -0500

    Update pg gem and add sync method

--- app/controllers/api/v1/contest_props_controller.rb
+++ app/controllers/api/v1/contest_props_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ContestPropsController < ApplicationController
 
   def index
+    sync_with_espn
     contest_week = ContestWeek.find_by(:id => params[:contest_week_id])
     if contest_week
       contest_props = ContestProp.filter_by_contest_week(contest_week)
@@ -11,4 +12,27 @@ class Api::V1::ContestPropsController < ApplicationController
     render json: contest_props, status: 200
 
   end
+
+  private
+
+  def sync_with_espn
+    espn = "http://fantasy.espn.com/streak/en/"
+    doc = Nokogiri::HTML(open(espn))
+    props = doc.css(".matchup-container")
+
+    props.each do |p|
+      date = p.css(".startTime").attr("data-locktime").value
+      contest_week = ContestWeek.find_or_create_by(name: "#{Date.today.cweek}")
+      ContestProp.create(
+        title:  p.css(".gamequestion").text,
+        date: DateTime.parse(date).in_time_zone('Eastern Time (US & Canada)').to_date,
+        start_time: DateTime.parse(date).in_time_zone('Eastern Time (US & Canada)'),
+        sport: p.css(".sport-description").text,
+        away_team: p.css("td span strong")[0].text,
+        home_team: p.css("td span strong")[1].text,
+        contest_week_id: contest_week.id
+      )
+
+
+    end
 end
