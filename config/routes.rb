ea8dbfe3728a7b5a2486a8b94ba9b05797935b20
John Blank <veloblank@gmail.com>
2019-11-29 13:09:53 -0500

    Generate user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
+      resources :users
       get '/current', to: "contest_boards#current"
       resources :contest_boards, only: [:show, :index] do
         resources :contest_props, only: [:show, :index]
