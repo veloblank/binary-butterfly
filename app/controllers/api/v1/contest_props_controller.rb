6165f57b0852103e1590200ee61d05325c72ad87
John Blank <veloblank@gmail.com>
2019-11-29 12:34:14 -0500

    Fixes api call and return correct sports props

--- app/controllers/api/v1/contest_props_controller.rb
+++ app/controllers/api/v1/contest_props_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::ContestPropsController < ApplicationController
 
   def index
+    contest_params = params[:contest_week_id]
     contest_week = ContestWeek.find_by(:id => params[:contest_week_id])
-    if contest_week
+    
+    if contest_params == "current"
+      contest_week = ContestWeek.all.last
+    elsif contest_week 
       contest_props = ContestProp.filter_by_contest_week(contest_week)
     else
       contest_props = ContestProp.all
