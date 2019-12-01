5d0014096ffdcf070b02a60733b3d8a20f907bc6
John Blank <veloblank@gmail.com>
2019-12-01 06:17:11 -0500

    Renders json with user data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params)
     #@user.create_pick_history
     if user.save
-      render json: {message: "User successfully created!", status: 201}
+      render json: {message: "User successfully created!", user: user, status: 201}
     else
       render json: {
         message: {
-          errors: user.errors.full_messages
+          errors: user.errors.full_messages,
         }
       }
     end
