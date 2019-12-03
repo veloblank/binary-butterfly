c163aa45c6b8115ce80868e1c06dcb5a296c68b4
John Blank <veloblank@gmail.com>
2019-12-03 06:33:39 -0500

    Render json w/o status message

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: 200
+      render json: user
     else
       render json: {
         message: {
