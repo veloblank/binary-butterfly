ead1a3b9779c3e07c5a99789b703c559462b0fd7
John Blank <veloblank@gmail.com>
2019-12-03 06:00:46 -0500

    Configure server response for error handling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    #@user.create_pick_history
     if user.save
-      render json: {message: "User successfully created!", user: user, status: 201}
+      render json: user, status: 200
     else
       render json: {
         message: {
-          errors: user.errors.full_messages,
-        }
+          errors: user.errors.full_messages
+        },
+        status: 204
       }
     end
   end
