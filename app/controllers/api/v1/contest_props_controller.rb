3217bbbe5690d4b58f2b1d568e3d60fe944be363
John Blank <veloblank@gmail.com>
2019-12-05 15:54:31 -0500

    Removes index action

--- app/controllers/api/v1/contest_props_controller.rb
+++ app/controllers/api/v1/contest_props_controller.rb
@@ -1,18 +1,2 @@
 class Api::V1::ContestPropsController < ApplicationController
-
-  def index
-    contest_params = params[:contest_board_id]
-    contest_board = ContestBoard.find_by(:id => params[:contest_board_id])
-    
-    if contest_params == "current"
-      contest_board = ContestBoard.all.last
-    elsif contest_board 
-      contest_props = ContestProp.filter_by_contest_board(contest_board)
-    else
-      contest_props = ContestProp.all
-    end
-
-    render json: contest_props
-
-  end
 end
