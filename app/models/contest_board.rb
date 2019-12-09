124c31b6e400ccdb8b32ea081fae7b82c25c7ee5
John Blank <veloblank@gmail.com>
2019-12-09 17:28:13 -0500

    Create validation for name uniqueness

--- app/models/contest_board.rb
+++ app/models/contest_board.rb
@@ -2,5 +2,6 @@ class ContestBoard < ApplicationRecord
   has_many :contest_props
   has_many :user_contest_boards
   has_many :users, through: :user_contest_boards
+  validates :name, uniqueness: true
 
 end
