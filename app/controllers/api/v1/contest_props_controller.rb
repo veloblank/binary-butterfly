dcb38ccf8bbaec481670f614867c58e307e329e7
John Blank <veloblank@gmail.com>
2019-12-09 05:27:39 -0500

    Build index action for props

--- app/controllers/api/v1/contest_props_controller.rb
+++ app/controllers/api/v1/contest_props_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::ContestPropsController < ApplicationController
+  def index
+    if params[:contest_board_id]
+      contest_board = ContestBoard.find_by(id: params[:contest_board_id])
+      if contest_board
+        contest_props = contest_board.contest_props
+        render json: contest_props
+      else
+        render json: {message: "The requested resource could not be found."}
+      end
+    else
+      contest_props = ContestProp.all
+      render json: contest_props
+    end
+  end
 end
