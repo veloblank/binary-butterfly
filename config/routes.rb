0c2eb852cf8df07cbba28747b1044ecf58f7b86c
John Blank <veloblank@gmail.com>
2019-12-11 12:21:10 -0500

    Builds nested resources for boards and props

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       get '/current', to: "contest_boards#current"
       resources :users, only: [:create, :show, :index] do
-        resources :user_picks, only: [:index]
+        resources :contest_boards, only: [:index, :show]
       end
       resources :contest_boards, only: [:show, :index, :create, :update, :destroy] do
         resources :contest_props, only: [:show, :index]
