c92feec2b366b08ca820224c2122fb6728fa0e85
John Blank <veloblank@gmail.com>
2019-12-13 20:33:43 -0500

    Create id specific class method

--- app/models/user_contest_board.rb
+++ app/models/user_contest_board.rb
@@ -3,7 +3,7 @@ class UserContestBoard < ApplicationRecord
   belongs_to :contest_board
   #has_many :user_picks
 
-  def self.day_specific_boards(id)
+  def self.id_specific_boards(id)
     UserContestBoard.where(contest_board_id: id)
   end
 end
