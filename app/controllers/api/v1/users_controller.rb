3abeccfe27e25c7acdf8bce7c8c2611cef694c23
John Blank <veloblank@gmail.com>
2019-12-13 20:34:16 -0500

    Remove redundant user_id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     #NO Authentication or Authorization is taking place
     if User.exists?(user.id)
       board = ContestBoard.last
-      user_board = user.user_contest_boards.find_or_create_by(user_id: user.id, contest_board_id: board.id)
+      user_board = user.user_contest_boards.find_or_create_by(contest_board_id: board.id)
       render json: {user: user, board: board.to_json(include: [:contest_props])}, status: 200
     elsif user.save
       board = ContestBoard.last
