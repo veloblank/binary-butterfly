dbe287c74a9fc08556dd423588c8b0febdbb5f2f
John Blank <veloblank@gmail.com>
2019-12-13 20:49:46 -0500

    UID specific class method

--- app/models/user_contest_board.rb
+++ app/models/user_contest_board.rb
@@ -3,7 +3,15 @@ class UserContestBoard < ApplicationRecord
   belongs_to :contest_board
   #has_many :user_picks
 
-  def self.id_specific_boards(id)
+  def self.cb_id_specific_boards(id)
     UserContestBoard.where(contest_board_id: id)
   end
+
+  def self.uid_specific_boards(id)
+    UserContestBoard.where(user_id: id)
+  end
+  
+  # def self.prop_specific_boards(prop_id)
+  #   UserContestBoard.where(prop_id: id)
+  # end
 end
