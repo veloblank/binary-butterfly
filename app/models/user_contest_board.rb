93cb9e4ce264a59bbfa5276c405591a98115fa19
John Blank <veloblank@gmail.com>
2019-12-13 20:33:17 -0500

    Create class query method

--- app/models/user_contest_board.rb
+++ app/models/user_contest_board.rb
@@ -1,4 +1,9 @@
 class UserContestBoard < ApplicationRecord
   belongs_to :user
   belongs_to :contest_board
+  #has_many :user_picks
+
+  def self.day_specific_boards(id)
+    UserContestBoard.where(contest_board_id: id)
+  end
 end
